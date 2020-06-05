3163dbfacc3d8e3f0c9718e011505cddf137487b
SabineSchropper <sabine.schropper@gmail.com>
2020-06-05 08:40:34 +0200

    first commit

---
+++ Supermarket/Supermarket.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Supermarket
+{
+    partial class Supermarket
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.tbProduct = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(46, 148);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(485, 276);
+            this.listBox.TabIndex = 0;
+            // 
+            // tbProduct
+            // 
+            this.tbProduct.Location = new System.Drawing.Point(125, 67);
+            this.tbProduct.Name = "tbProduct";
+            this.tbProduct.Size = new System.Drawing.Size(205, 22);
+            this.tbProduct.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Produkt:";
+            // 
+            // tbAmount
+            // 
+            this.tbAmount.Location = new System.Drawing.Point(447, 67);
+            this.tbAmount.Name = "tbAmount";
+            this.tbAmount.Size = new System.Drawing.Size(84, 22);
+            this.tbAmount.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(369, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Anzahl:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(562, 60);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(102, 36);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Hinzufügen";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(679, 60);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(90, 36);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Entfernen";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Supermarket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbAmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbProduct);
+            this.Controls.Add(this.listBox);
+            this.Name = "Supermarket";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox tbProduct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ Supermarket/Supermarket.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Supermarket
+{
+    public partial class Supermarket : Form
+    {
+        Dictionary<string, int> productsDict = new Dictionary<string, int>();
+        public Supermarket()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(tbProduct.Text) && !string.IsNullOrEmpty(tbAmount.Text))
+            {
+                int amount = 0;
+                string product = tbProduct.Text;
+                bool success = Int32.TryParse(tbAmount.Text, out amount);
+                if (success)
+                {
+                    if (productsDict.ContainsKey(product))
+                    {
+                        productsDict[product] += amount;
+                    }
+                    else
+                    {
+                        productsDict.Add(product, amount);
+                    } 
+                }
+                else
+                {
+                    Console.WriteLine("Fehler beim Umwandeln in eine Zahl.");
+                }
+
+                UpdateChanges();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(tbProduct.Text) && !string.IsNullOrEmpty(tbAmount.Text))
+            {
+                int amount = 0;
+                string product = tbProduct.Text;
+                bool isRemoved = false;
+                bool success = Int32.TryParse(tbAmount.Text, out amount);
+                if (success)
+                {
+                    if (productsDict.ContainsKey(product))
+                    {
+                        if(amount < productsDict[product] && amount > 0)
+                        {
+                            productsDict[product] -= amount;
+                        }
+                        if(amount == productsDict[product])
+                        {
+                            isRemoved = productsDict.Remove(product);
+                        }
+                    }
+                 
+                }
+                else
+                {
+                    Console.WriteLine("Fehler beim Umwandeln in eine Zahl.");
+                }
+
+                UpdateChanges();
+            }
+
+        }
+
+        private void UpdateChanges()
+        {
+            List<string> productList = new List<string>();
+            foreach (var item in productsDict)
+            {
+                productList.Add(item.Key + "(" + item.Value + ")");
+            }
+            listBox.DataSource = productList;
+        }
+    }
+}
